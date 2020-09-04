8c21db2830cff202aea3642623e239da7ccb6825
douch <douch@163.com>
2020-09-04 16:57:02 +0800

    增加phong光照模型

---
+++ 05/OpenlGL01/Camera.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Camera
+{
+public:
+	Camera(glm::vec3 position,glm::vec3 target,glm::vec3 worldUp);
+	Camera(glm::vec3 position, float pitch, float yaw, glm::vec3 worldUp);
+	~Camera();
+
+	glm::vec3 Position;
+	glm::vec3 Forward;
+	glm::vec3 Right;
+	glm::vec3 Up;
+	glm::vec3 WorldUp;
+
+	float Yaw;
+	float Pitch;
+
+	float senseX=0.01f;
+	float senseY = 0.01f;
+
+	float speedY = 0.0f;
+	float speedZ = 0.0f;
+	float speedX = 0.0f;
+	glm::mat4 GetViewMat();
+	void ProcessMoiseMovement(float deltaX, float deltaY);
+	void UpdateCameraPos();
+private:
+	void UpdateCameraVectors();
+};
